4c200ca43807314fe4d5ed5defde788525ba1a92
jana-sw <74660676+jana-sw@users.noreply.github.com>
2020-12-29 19:40:35 +0100

    dodałam spacje. Sprawdzam komitowanie na gałęziach

--- src/gauss.c
+++ src/gauss.c
@@ -4,25 +4,24 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
- 	int row = mat -> r;
+int eliminate (Matrix *mat, Matrix *b) {
+ 	
+	int row = mat -> r;
 	int col  = mat -> c;
-	for (int k = 0; k < col -1; k++) {
+	for (int k = 0; k < col - 1; k++) {
+	
 	    for ( int w = k +1 ; w < row; w++) {
 		double wsp =  mat ->data[w][k]  /  mat -> data[k][k] ;
-		for ( int j = k ; j < col; j++) {
+	
+		for ( int j = k ; j < col; j++) 
 			mat -> data[w][j] -= mat -> data[k][j]*wsp;
 				
-	    }
+	  
 		b -> data[w][0] -=  b -> data[k][0] * wsp;
 		
 		}
 	}
 
-   /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+	return 0;
 }
 
