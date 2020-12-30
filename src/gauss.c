528f44152d4f4dcbd7dad8e0cc8b95173bdcd613
jana-sw <74660676+jana-sw@users.noreply.github.com>
2020-12-30 16:06:00 +0100

    Program działa z wybieraniem największego elementu na przekątnej. Obsługuje błędy.

--- src/gauss.c
+++ src/gauss.c
@@ -8,18 +8,39 @@ int eliminate (Matrix *mat, Matrix *b) {
  	
 	int row = mat -> r;
 	int col  = mat -> c;
-	for (int k = 0; k < col - 1; k++) {
-	
-	    for ( int w = k +1 ; w < row; w++) {
-		double wsp =  mat ->data[w][k]  /  mat -> data[k][k] ;
-	
-		for ( int j = k ; j < col; j++) 
-			mat -> data[w][j] -= mat -> data[k][j]*wsp;
-				
-	  
-		b -> data[w][0] -=  b -> data[k][0] * wsp;
-		
+ 	for (int k = 0; k < col - 1; k++) {
+		int wmax = k;
+
+		for (int i = k + 1; i < col; i++)
+			if (mat -> data[k][i] >  mat -> data[k][k])
+					wmax = i;
+			
+        
+		if (mat -> data[k][wmax] == 0)
+			return 1;
+ 
+		if (wmax != k) {
+			double  *rowtmp = mat -> data[k]; // zamieniamy miejscami wiersze z większymi w
+			mat -> data[k] = mat -> data[wmax];
+			mat -> data[wmax] = rowtmp;
+			
+			double btmp = b -> data[k][0];
+			b -> data[k][0] = b -> data[wmax][0];
+			b -> data[wmax][0] = btmp;
 		}
+	
+		for ( int w = k +1 ; w < row; w++) {
+			double wsp =  mat ->data[w][k]  /  mat -> data[k][k] ;
+	
+			for ( int j = k ; j < col; j++) 
+				mat -> data[w][j] -= mat -> data[k][j]*wsp;
+			
+			//printf ("%lf ", mat -> data[w][k]);
+	  
+			b -> data[w][0] -=  b -> data[k][0] * wsp;
+		}
+		//printf("\n");
+ 	
 	}
 
 	return 0;
